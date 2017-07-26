af8bb56dfa00adf78cda92f893d20b562e6f380f
Menem Ragab <menem@asknative.com>
2017-07-26 17:47:41 +0200

    FEAT: Initial Commit

---
+++ LetsGo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LetsGo
+//
+//  Created by Menem Ragab on 7/26/17.
+//  Copyright © 2017 Phoenix fitness. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
