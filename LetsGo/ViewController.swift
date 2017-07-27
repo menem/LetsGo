16eb363a4643187eecf1ff40efbcde966a677e09
Menem Ragab <menem@asknative.com>
2017-07-27 14:55:47 +0200

    FEAT: Interval Setup

--- LetsGo/ViewController.swift
+++ LetsGo/ViewController.swift
@@ -10,41 +10,42 @@ import UIKit
 import MZTimerLabel
 import KYNavigationProgress
 import HGCircularSlider
-
+import AVFoundation
 class ViewController: UIViewController {
 
+    var intervals = 0
+    var timer: MZTimerLabel!
+    var intervalsLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-//        // Do any additional setup after loading the view, typically from a nib.
-//        MZTimerLabel *timer = [[MZTimerLabel alloc] initWithLabel:aUILabel andTimerType:MZTimerLabelTypeTimer];
-//        [timer setCountDownTime:60];
-//        [timer start];
-        
-        
-        
-        // get and set progress.
-        
-        let circularSlider = RangeCircularSlider(frame: CGRect(x: 0, y: self.view.frame.size.height/2, width: self.view.frame.size.width, height: 200))
-        circularSlider.startThumbImage = UIImage(named: "Bedtime")
-        circularSlider.endThumbImage = UIImage(named: "Wake")
-        circularSlider.backgroundColor = .clear
-        let dayInSeconds = 24 * 60 * 60
-        circularSlider.maximumValue = CGFloat(dayInSeconds)
-        
-        circularSlider.startPointValue = 1 * 60 * 60
-        circularSlider.endPointValue = 8 * 60 * 60
-//        circularSlider.numberOfRounds = 2 // Two rotations for full 24h range
-        
-        self.view.addSubview(circularSlider)
-        
+
+        intervals = 10
         let timerLabel = UILabel(frame: CGRect(x: 0, y: (self.view.frame.size.height/2)+40, width: self.view.frame.size.width, height: 40))
         timerLabel.textAlignment = .center
-        let timer = MZTimerLabel(label: timerLabel, andTimerType: MZTimerLabelTypeTimer)
-        timer?.setCountDownTime(50)
+        timerLabel.font = UIFont (name: "Avenir-Book", size: 30)
+        timerLabel.textColor = UIColor(red:0.83, green:0.00, blue:0.00, alpha:1.00)
+        
+        intervalsLabel = UILabel(frame: CGRect(x: 0, y: (self.view.frame.size.height/2), width: self.view.frame.size.width, height: 40))
+        intervalsLabel.textAlignment = .center
+        intervalsLabel.font = UIFont (name: "Avenir-Book", size: 30)
+        intervalsLabel.textColor = UIColor(red:0.83, green:0.00, blue:0.00, alpha:1.00)
+        intervalsLabel.text = "Intervals = \(intervals)"
+        
+        
+        timer = MZTimerLabel(label: timerLabel, andTimerType: MZTimerLabelTypeTimer)
+        timer?.setCountDownTime(60)
+        timer?.resetTimerAfterFinish = true
+        
         self.view.addSubview(timerLabel)
+        self.view.addSubview(intervalsLabel)
+        
         self.navigationController?.progressTintColor = UIColor(red:0.83, green:0.00, blue:0.00, alpha:1.00)
         timer?.delegate = self
-        timer?.start()
+        
+        startInterval()
+        
+
     }
     
 
@@ -52,17 +53,26 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func startInterval(){
+        // to play sound
+        let systemSoundID: SystemSoundID = 1304
+        AudioServicesPlaySystemSound (systemSoundID)
+        timer.start()
+        intervals -= 1
+    }
 
 }
-
 extension ViewController: MZTimerLabelDelegate {
     func timerLabel(_ timerLabel: MZTimerLabel!, countingTo time: TimeInterval, timertype timerType: MZTimerLabelType){
         let progress = time/timerLabel.getCountDownTime()
         self.navigationController?.progress = Float(progress)
+        timerLabel.text = "Intervals = \(intervals)"
     }
     
     func timerLabel(_ timerLabel: MZTimerLabel!, finshedCountDownTimerWithTime countTime: TimeInterval){
-        
+        if (intervals > 0){
+        startInterval()
+        }
     }
 }
