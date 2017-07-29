9852d810d25a648ae8feadb93813399087ca9273
Menem Ragab <menem@asknative.com>
2017-07-29 04:41:13 +0200

    FEAT: tabata is now in business

--- LetsGo/ViewController.swift
+++ LetsGo/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
     var intervals = 0
     var timer: MZTimerLabel!
     var intervalsLabel: UILabel!
-    var circleSlider: CircularSlider!
+    var IntervalcircleSlider: CircularSlider!
     var timeLabel: UILabel!
     var userIntent: INStartWorkoutIntent!
     var clockLabel: UILabel!
@@ -27,8 +27,20 @@ class ViewController: UIViewController {
     var selectedIndex = 0
     var titleView: UILabel!
     var isInterval: Bool!
+    var onInterval = 0.0
+    var offInterval = 0.0
+    var ontimer: MZTimerLabel!
+    var offtimer: MZTimerLabel!
+    var roundsCircleSlider: CircularSlider!
+    var onCircleSlider: CircularSlider!
+    var offCircleSlider: CircularSlider!
     
     @IBAction func startIntervalPressed(_ sender: Any) {
+        let selectedMode = modes[selectedIndex]
+        if (selectedMode == "tabata"){
+            ontimer.start()
+            return
+        }
         startInterval()
     }
     
@@ -79,7 +91,14 @@ class ViewController: UIViewController {
     }
     
     func configuretimer() {
-        
+        ontimer = MZTimerLabel(label: timeLabel, andTimerType: MZTimerLabelTypeTimer)
+        //        timer?.setCountDownTime(60)
+        ontimer?.resetTimerAfterFinish = true
+        ontimer?.delegate = self
+        offtimer = MZTimerLabel(label: timeLabel, andTimerType: MZTimerLabelTypeTimer)
+        //        timer?.setCountDownTime(60)
+        offtimer?.resetTimerAfterFinish = true
+        offtimer?.delegate = self
         
         if ((userIntent) != nil){
             switch String(describing: userIntent.workoutName).lowercased() {
@@ -138,6 +157,10 @@ class ViewController: UIViewController {
     }
     
     func startInterval(){
+        
+
+
+        
         let systemSoundID: SystemSoundID = 1304
         AudioServicesPlaySystemSound (systemSoundID)
         timer?.start()
@@ -145,6 +168,13 @@ class ViewController: UIViewController {
     }
     
     func stopTimer(){
+        let selectedMode = modes[selectedIndex]
+        if (selectedMode == "tabata"){
+            ontimer.pause()
+            ontimer.reset()
+            offtimer.pause()
+            offtimer.reset()
+        }
         timer.pause()
         timer.reset()
     }
@@ -157,9 +187,20 @@ class ViewController: UIViewController {
             timer = MZTimerLabel(label: timeLabel, andTimerType: MZTimerLabelTypeStopWatch)
             timer?.delegate = self
             isInterval = false
-            circleSlider.title = "Time cap"
-            circleSlider.divisa = "Min"
+            IntervalcircleSlider.title = "Time cap"
+            IntervalcircleSlider.divisa = "Min"
             intervalsLabel.isHidden = true
+            IntervalcircleSlider.isHidden = false
+            if (self.view.subviews.contains(roundsCircleSlider)){
+                roundsCircleSlider.removeFromSuperview()
+            }
+            if (self.view.subviews.contains(onCircleSlider)){
+                onCircleSlider.removeFromSuperview()
+            }
+            if (self.view.subviews.contains(offCircleSlider)){
+                offCircleSlider.removeFromSuperview()
+            }
+            
         case "timer":
             print("Timer Selected")
             let totalTime = intervals * 60
@@ -167,11 +208,27 @@ class ViewController: UIViewController {
             timer?.delegate = self
             timer?.setCountDownTime(TimeInterval(totalTime))
             isInterval = false
-            circleSlider.title = "Time cap"
-            circleSlider.divisa = "Min"
-            intervalsLabel.isHidden = false
+            IntervalcircleSlider.title = "Time cap"
+            IntervalcircleSlider.divisa = "Min"
+            intervalsLabel.isHidden = true
+            IntervalcircleSlider.isHidden = false
+            if (self.view.subviews.contains(roundsCircleSlider)){
+                roundsCircleSlider.removeFromSuperview()
+            }
+            if (self.view.subviews.contains(onCircleSlider)){
+                onCircleSlider.removeFromSuperview()
+            }
+            if (self.view.subviews.contains(offCircleSlider)){
+                offCircleSlider.removeFromSuperview()
+            }
         case "tabata":
             print("tabata Selected")
+            IntervalcircleSlider.isHidden = true
+            intervalsLabel.isHidden = false
+
+            self.view.addSubview(onCircleSlider)
+            self.view.addSubview(offCircleSlider)
+            self.view.addSubview(roundsCircleSlider)
             
         default:
             print("default Selected")
@@ -181,18 +238,18 @@ class ViewController: UIViewController {
         }
     }
     func makeUserInterface(){
-        circleSlider = CircularSlider(frame:CGRect(x: (self.view.frame.size.width/2)-125, y: 140, width: 250, height: 250))
-        circleSlider.delegate = self
-        circleSlider.maximumValue = 120
-        circleSlider.minimumValue = 1
-        circleSlider.value = 1
-        circleSlider.knobRadius = 20
-        circleSlider.radiansOffset = 0.01
-        circleSlider.backgroundColor = .clear
-        circleSlider.pgHighlightedColor = UIColor(red:0.83, green:0.00, blue:0.00, alpha:1.00)
-        circleSlider.pgNormalColor = UIColor(red:0.95, green:0.95, blue:0.95, alpha:1.00)
-        circleSlider.title = "Time"
-        circleSlider.divisa = "Min"
+        IntervalcircleSlider = CircularSlider(frame:CGRect(x: (self.view.frame.size.width/2)-125, y: 140, width: 250, height: 250))
+        IntervalcircleSlider.delegate = self
+        IntervalcircleSlider.maximumValue = 120
+        IntervalcircleSlider.minimumValue = 1
+        IntervalcircleSlider.value = 1
+        IntervalcircleSlider.knobRadius = 20
+        IntervalcircleSlider.radiansOffset = 0.01
+        IntervalcircleSlider.backgroundColor = .clear
+        IntervalcircleSlider.pgHighlightedColor = UIColor(red:0.83, green:0.00, blue:0.00, alpha:1.00)
+        IntervalcircleSlider.pgNormalColor = UIColor(red:0.95, green:0.95, blue:0.95, alpha:1.00)
+        IntervalcircleSlider.title = "Time"
+        IntervalcircleSlider.divisa = "Min"
         
         timeLabel = UILabel(frame: CGRect(x: 0, y: (self.view.frame.size.height/2)+120, width: self.view.frame.size.width, height: 60))
         timeLabel.textAlignment = .center
@@ -212,9 +269,58 @@ class ViewController: UIViewController {
         clockLabel.textColor = UIColor(red:0.83, green:0.00, blue:0.00, alpha:1.00)
         
         
+        roundsCircleSlider = CircularSlider(frame:CGRect(x: (self.view.frame.size.width/2)-94, y: 100, width: 180, height: 180))
+        roundsCircleSlider.delegate = self
+        roundsCircleSlider.maximumValue = 60
+        roundsCircleSlider.minimumValue = 1
+        roundsCircleSlider.value = 1
+        roundsCircleSlider.knobRadius = 20
+        roundsCircleSlider.radiansOffset = 0.01
+        roundsCircleSlider.backgroundColor = .clear
+        roundsCircleSlider.pgHighlightedColor = UIColor(red:0.83, green:0.00, blue:0.00, alpha:1.00)
+        roundsCircleSlider.pgNormalColor = UIColor(red:0.95, green:0.95, blue:0.95, alpha:1.00)
+        roundsCircleSlider.title = "Rounds"
+        roundsCircleSlider.divisa = ""
+        
+        onCircleSlider = CircularSlider(frame:CGRect(x: 20, y: 280, width: 180, height: 180))
+        onCircleSlider.delegate = self
+        onCircleSlider.maximumValue = 5
+        onCircleSlider.minimumValue = 0.1
+        onCircleSlider.value = 0.1
+        
+        onCircleSlider.knobRadius = 20
+        onCircleSlider.radiansOffset = 0.01
+        onCircleSlider.backgroundColor = .clear
+        onCircleSlider.pgHighlightedColor = UIColor(red:0.83, green:0.00, blue:0.00, alpha:1.00)
+        onCircleSlider.pgNormalColor = UIColor(red:0.95, green:0.95, blue:0.95, alpha:1.00)
+        onCircleSlider.title = "ON"
+        onCircleSlider.divisa = "Min"
+        ontimer = MZTimerLabel(label: timeLabel, andTimerType: MZTimerLabelTypeTimer)
+        ontimer?.delegate = self
+        ontimer.tag = 1
+        //            timer?.setCountDownTime(TimeInterval(totalTime))
+        
+        offCircleSlider = CircularSlider(frame:CGRect(x: (self.view.frame.size.width)-200, y: 280, width: 180, height: 180))
+        offCircleSlider.delegate = self
+        offCircleSlider.maximumValue = 5
+        offCircleSlider.minimumValue = 0.1
+        offCircleSlider.value = 0.1
+        offCircleSlider.knobRadius = 20
+        offCircleSlider.radiansOffset = 0.01
+        offCircleSlider.backgroundColor = .clear
+        offCircleSlider.pgHighlightedColor = UIColor(red:0.83, green:0.00, blue:0.00, alpha:1.00)
+        offCircleSlider.pgNormalColor = UIColor(red:0.95, green:0.95, blue:0.95, alpha:1.00)
+        offCircleSlider.title = "OFF"
+        offCircleSlider.divisa = "Min"
+        offtimer = MZTimerLabel(label: timeLabel, andTimerType: MZTimerLabelTypeTimer)
+        offtimer?.delegate = self
+        offtimer.tag = 2
+        //            offtimer?.setCountDownTime(TimeInterval(totalTime))
+        
+        
         self.view.addSubview(timeLabel)
         self.view.addSubview(intervalsLabel)
-        self.view.addSubview(self.circleSlider)
+        self.view.addSubview(self.IntervalcircleSlider)
         self.view.addSubview(clockLabel)
         
         clockTimer = Timer.scheduledTimer(timeInterval: 1.0,
@@ -231,6 +337,22 @@ class ViewController: UIViewController {
                                                         timeStyle: .medium)
     }
     
+    func startONTimer(){
+        let systemSoundID: SystemSoundID = 1304
+        AudioServicesPlaySystemSound (systemSoundID)
+        ontimer?.start()
+        timeLabel.textColor = UIColor(red:0.83, green:0.00, blue:0.00, alpha:1.00)
+        
+//        intervals -= 1
+    }
+    func startOFFTimer(){
+        let systemSoundID: SystemSoundID = 1304
+        AudioServicesPlaySystemSound (systemSoundID)
+        offtimer?.start()
+        timeLabel.textColor = UIColor(red:0.00, green:0.92, blue:0.78, alpha:1.00)
+       
+    }
+    
 }
 extension ViewController: MZTimerLabelDelegate {
     func timerLabel(_ timerLabel: MZTimerLabel!, countingTo time: TimeInterval, timertype timerType: MZTimerLabelType){
@@ -241,13 +363,42 @@ extension ViewController: MZTimerLabelDelegate {
     
     func timerLabel(_ timerLabel: MZTimerLabel!, finshedCountDownTimerWithTime countTime: TimeInterval){
         if (intervals > 0){
+            if (timerLabel.tag == 1){
+                ontimer.pause()
+                ontimer.reset()
+            startOFFTimer()
+            return
+            }
+            if (timerLabel.tag == 2){
+                offtimer.pause()
+                offtimer.reset()
+            startONTimer()
+            intervals -= 1
+                return
+            }
             startInterval()
         }
     }
 }
+func round(_ value: Double, toNearest: Double) -> Double {
+    return round(value / toNearest) * toNearest
+}
 
 extension ViewController: CircularSliderDelegate {
     func circularSlider(_ circularSlider: CircularSlider, valueForValue value: Float) -> Float {
+        if (circularSlider.title == "ON"){
+            onInterval = round(Double(value), toNearest:0.1)
+            let onTime = onInterval * 60
+            ontimer?.setCountDownTime(TimeInterval(onTime))
+            return Float(onInterval)
+        }
+        
+        if (circularSlider.title == "OFF"){
+            offInterval = round(Double(value), toNearest:0.1)
+            let offTime = offInterval * 60
+            offtimer?.setCountDownTime(TimeInterval(offTime))
+            return Float(offInterval)
+        }
         
         if ((userIntent) != nil){
             let minute = userIntent.goalValue!/60
