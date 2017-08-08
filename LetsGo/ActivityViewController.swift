be66cc812207820c6cccc9c2228e7afddf493345
Menem Ragab <menem@asknative.com>
2017-08-08 05:52:57 +0200

    FEAT: Activity Creation and play

--- LetsGo/ActivityViewController.swift
+++ LetsGo/ActivityViewController.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import MZTimerLabel
+import KYNavigationProgress
+import AVFoundation
 
 let TimerTableViewCellIdentifier = "TimerTableViewCellIdentifier"
 
@@ -14,6 +17,11 @@ class ActivityViewController: UITableViewController {
     
     var timers = [LGTimer]()
     var activity: LGActivity!
+    var timer: MZTimerLabel!
+    var timeLabel: UILabel!
+    var currentlyPlaying: Int!
+    var currentInterval: Int!
+    var player: AVAudioPlayer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,16 +31,54 @@ class ActivityViewController: UITableViewController {
         self.tableView.separatorStyle = .none
         self.tableView.register(TitleBackgroundTableViewCell.self, forCellReuseIdentifier: BannerTableViewCellIdentifier)
         self.title = activity.title
+        self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName : UIColor.white]
         
         let timeManager = LGTimerManager()
         timers = timeManager.loadTimers(activity: activity)
         self.tableView.reloadData()
         
+        let playBarButton = UIBarButtonItem(title: "Play", style: .plain, target: self, action: #selector(startActivity))
+        self.navigationItem.rightBarButtonItem = playBarButton
         
+        
+        timeLabel = UILabel(frame: CGRect(x:0, y: (self.view.frame.size.height)-160, width: self.view.frame.size.width, height: 120))
+        timeLabel.textAlignment = .center
+        timeLabel.font = UIFont (name: "Avenir-Heavy", size: 80)
+        timeLabel.textColor = #colorLiteral(red: 0.340575099, green: 0.3558157086, blue: 0.4202301502, alpha: 1)
+        
+        timer = MZTimerLabel(label: timeLabel, andTimerType: MZTimerLabelTypeTimer)
+        timer?.setCountDownTime(calculateTotalTime())
+        timer?.resetTimerAfterFinish = true
+        timer?.delegate = self
+        
+        self.view.addSubview(timeLabel)
     }
     
     // MARK: - Table view data source
-    
+    func startActivity(){
+        
+        currentlyPlaying = 0
+        timer.setCountDownTime((timers.first?.duration)!)
+        currentInterval = timers.first?.intervals
+        self.title = timers.first?.title
+        playSound()
+        timer.start()
+    }
+    func resumeActivity(){
+        let timerDuration = timers[currentlyPlaying].duration
+        currentInterval = timers[currentlyPlaying].intervals
+        timer.setCountDownTime(timerDuration)
+        self.title = timers[currentlyPlaying].title
+        playSound()
+        timer.start()
+    }
+    func resumeTimer(index: Int){
+        let timerDuration = timers[index].duration
+        timer.setCountDownTime(timerDuration)
+        self.title = timers[index].title
+        playSound()
+        timer.start()
+    }
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 2
         
@@ -85,6 +131,15 @@ class ActivityViewController: UITableViewController {
         
     }
     
+    func calculateTotalTime() -> Double {
+        var accumelatedTime = 0.0
+        for timer in timers {
+            
+        accumelatedTime += timer.duration.multiplied(by:Double(timer.intervals))
+        }
+        return accumelatedTime
+    }
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if (indexPath.section == 0) {
             let newTimerViewController = NewTimerViewController()
@@ -93,5 +148,43 @@ class ActivityViewController: UITableViewController {
         }
     }
     
+    func playSound() {
+        guard let url = Bundle.main.url(forResource: "tone", withExtension: "wav") else { return }
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            player = try AVAudioPlayer(contentsOf: url)
+            guard let player = player else { return }
+            
+            player.play()
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+}
+
+extension ActivityViewController: MZTimerLabelDelegate {
+    func timerLabel(_ timerLabel: MZTimerLabel!, countingTo time: TimeInterval, timertype timerType: MZTimerLabelType){
+        let progress = time/timerLabel.getCountDownTime()
+        self.navigationController?.progress = Float(progress)        
+    }
     
+    func timerLabel(_ timerLabel: MZTimerLabel!, finshedCountDownTimerWithTime countTime: TimeInterval){
+         currentInterval! -= 1
+        if (currentInterval > 0) {
+            resumeTimer(index: currentlyPlaying)
+            return
+        }
+        if (currentlyPlaying < timers.count){
+            currentlyPlaying! += 1
+            if currentlyPlaying == timers.count {
+                return
+            }
+            resumeActivity()
+            return
+        }
+        
+    }
 }
