483e2d0af65d7be6492093ae23a1ee71f899c2f5
Menem Ragab <menem@asknative.com>
2017-08-12 00:38:22 +0200

    FEAT: Duration Selection View UI Enhancements

--- LetsGo/LGDurationSelection.swift
+++ LetsGo/LGDurationSelection.swift
@@ -13,7 +13,19 @@ import HGCircularSlider
 class LGDurationSelection: UIView {
   
     lazy var minutesCircularSlider: CircularSlider = {
-       let circularSlider = CircularSlider(frame: CGRect(x: 20, y: 20, width: 150, height: 150))
+       let circularSlider = CircularSlider(frame: CGRect(x: 20, y: 20, width: 250, height: 250))
+        circularSlider.diskColor = .clear
+        circularSlider.diskFillColor = .clear
+        circularSlider.trackFillColor = #colorLiteral(red: 0.1977134943, green: 0.2141624689, blue: 0.2560140491, alpha: 1)
+        circularSlider.trackColor = .clear
+        circularSlider.backgroundColor = .clear
+        circularSlider.lineWidth = 4
+        circularSlider.thumbLineWidth = 4
+        circularSlider.thumbRadius = 4
+        circularSlider.endThumbTintColor = #colorLiteral(red: 0.1977134943, green: 0.2141624689, blue: 0.2560140491, alpha: 1)
+        circularSlider.endThumbStrokeHighlightedColor = .clear
+        circularSlider.endThumbStrokeColor = .clear
+        
         circularSlider.minimumValue = 0
         circularSlider.maximumValue = 60
         circularSlider.endPointValue = 35
@@ -24,7 +36,20 @@ class LGDurationSelection: UIView {
     
     
     lazy var hoursCircularSlider: CircularSlider = {
-        let circularSlider =  CircularSlider(frame: CGRect(x: 40, y: 40, width: 100, height: 100))
+        let circularSlider =  CircularSlider(frame: CGRect(x: 50, y: 50, width: 190, height: 190))
+        circularSlider.diskColor = .clear
+        circularSlider.diskFillColor = .clear
+        circularSlider.trackFillColor = #colorLiteral(red: 0.1977134943, green: 0.2141624689, blue: 0.2560140491, alpha: 1)
+        circularSlider.trackColor = .clear
+        circularSlider.backgroundColor = .clear
+        circularSlider.lineWidth = 4
+        circularSlider.thumbLineWidth = 4
+        circularSlider.thumbRadius = 4
+        circularSlider.endThumbTintColor = #colorLiteral(red: 0.1977134943, green: 0.2141624689, blue: 0.2560140491, alpha: 1)
+        circularSlider.endThumbStrokeHighlightedColor = .clear
+        circularSlider.endThumbStrokeColor = .clear
+        
+        
         circularSlider.minimumValue = 0
         circularSlider.maximumValue = 12
         circularSlider.endPointValue = 6
@@ -33,20 +58,20 @@ class LGDurationSelection: UIView {
         return circularSlider
     }()
     
-    lazy var hoursLabel: UILabel = {
-        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 250, height: 120))
+    var hoursLabel: UILabel = {
+        let label = UILabel(frame: CGRect(x: 200, y: 270, width: 60, height: 120))
         label.textAlignment = .center
-        label.font = UIFont (name: "Betm-Regular3", size: 60)
-        label.textColor = #colorLiteral(red: 1, green: 0.4542224407, blue: 0.1010893807, alpha: 1)
+        label.font = UIFont (name: "Betm-Regular3", size: 25)
+        label.textColor = #colorLiteral(red: 0.1977134943, green: 0.2141624689, blue: 0.2560140491, alpha: 1)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
-    lazy var minutesLabel: UILabel = {
-        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 250, height: 120))
+    var minutesLabel: UILabel = {
+        let label = UILabel(frame: CGRect(x: 260, y: 270, width: 60, height: 40))
         label.textAlignment = .center
-        label.font = UIFont (name: "Betm-Regular3", size: 60)
-        label.textColor = #colorLiteral(red: 1, green: 0.4542224407, blue: 0.1010893807, alpha: 1)
+        label.font = UIFont (name: "Betm-Regular3", size: 25)
+        label.textColor = #colorLiteral(red: 0.1977134943, green: 0.2141624689, blue: 0.2560140491, alpha: 1)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
@@ -57,8 +82,8 @@ class LGDurationSelection: UIView {
         
         self.addSubview(minutesCircularSlider)
         self.addSubview(hoursCircularSlider)
-//        self.addSubview(hoursLabel)
-//        self.addSubview(minutesLabel)
+        self.addSubview(hoursLabel)
+        self.addSubview(minutesLabel)
         
         setNeedsUpdateConstraints()
         
@@ -74,14 +99,21 @@ class LGDurationSelection: UIView {
             minutesCircularSlider.centerYAnchor.constraint(equalTo: self.centerYAnchor),
             minutesCircularSlider.widthAnchor.constraint(equalTo: self.widthAnchor, constant: -20),
             minutesCircularSlider.heightAnchor.constraint(equalTo: self.heightAnchor, constant: -20),
-                        
+            
             hoursCircularSlider.centerXAnchor.constraint(equalTo: self.centerXAnchor),
             hoursCircularSlider.centerYAnchor.constraint(equalTo: self.centerYAnchor),
-            hoursCircularSlider.widthAnchor.constraint(equalTo: self.widthAnchor, constant: -20),
-            hoursCircularSlider.heightAnchor.constraint(equalTo: self.heightAnchor, constant: -20)
+            hoursCircularSlider.widthAnchor.constraint(equalTo: self.widthAnchor, constant: -40),
+            hoursCircularSlider.heightAnchor.constraint(equalTo: self.heightAnchor, constant: -40),
             
+            hoursLabel.centerXAnchor.constraint(equalTo: hoursCircularSlider.centerXAnchor, constant: -20),
+            hoursLabel.centerYAnchor.constraint(equalTo: hoursCircularSlider.centerYAnchor),
+            hoursLabel.widthAnchor.constraint(equalTo: self.widthAnchor, constant: -40),
+            hoursLabel.heightAnchor.constraint(equalTo: self.heightAnchor, constant: -40),
             
-            
+            minutesLabel.centerXAnchor.constraint(equalTo: hoursCircularSlider.centerXAnchor, constant: 20),
+            minutesLabel.centerYAnchor.constraint(equalTo: hoursCircularSlider.centerYAnchor),
+            minutesLabel.widthAnchor.constraint(equalTo: self.widthAnchor, constant: -40),
+            minutesLabel.heightAnchor.constraint(equalTo: self.heightAnchor, constant: -40)
             ])
         super.updateConstraints()
     }
