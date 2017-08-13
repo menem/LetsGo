b3b4ee0171dd7e93cd62e359b0301c0d4bf3e994
Menem Ragab <menem@asknative.com>
2017-08-13 10:23:42 +0200

    FEAT: adds intervalsViewController

--- LetsGo/HomeViewController.swift
+++ LetsGo/HomeViewController.swift
@@ -14,15 +14,20 @@ class HomeViewController: EZSwipeController {
     override func setupView() {
         datasource = self
     }
+    
+    func pushActivities(){
+        let activitiesViewController = ActivitiesViewController()
+        self.navigationController?.pushViewController(activitiesViewController, animated: true)
+    }
 }
 
 extension HomeViewController: EZSwipeControllerDataSource {
     func viewControllerData() -> [UIViewController] {
         let timerViewController = TimerViewController()
         let stopViewController = StopwatchViewController()
-        let activitiesViewController = ActivitiesViewController()
+        let intervalsViewController = IntervalsViewController()
         
-        return [timerViewController, stopViewController,activitiesViewController ]
+        return [timerViewController, stopViewController,intervalsViewController]
     }
     
     func titlesForPages() -> [String] {
@@ -30,11 +35,44 @@ extension HomeViewController: EZSwipeControllerDataSource {
     }
     
     func indexOfStartingPage() -> Int {
-        return 1 // EZSwipeController starts from 2nd, green page
+        return 1
     }
     
     func changedToPageIndex(_ index: Int) {
         // You can do anything from here, for now we'll just print the new index
         print(index)
     }
+
+    func navigationBarDataForPageIndex(_ index: Int) -> UINavigationBar {
+        var title = ""
+        switch index {
+        case 1:
+            title = "Stopwatch"
+        case 2:
+            title = "Tabata"
+        default:
+             title = "Timer"
+        }
+        
+
+        let navigationBar = UINavigationBar()
+         navigationBar.barStyle = UIBarStyle.default
+        let navigationItem = UINavigationItem(title: title)
+        navigationItem.hidesBackButton = true
+        
+        let rightBarButton = UIBarButtonItem(image: UIImage(named: "icn_activities"), style: .plain, target: self, action: #selector(pushActivities))
+        navigationItem.leftBarButtonItem = nil
+        navigationItem.rightBarButtonItem = rightBarButton
+        navigationBar.pushItem(navigationItem, animated: false)
+        return navigationBar
+    }
+
+    func disableSwipingForRightButtonAtPageIndex(_ index: Int) -> Bool {
+         return false
+    }
+    func clickedRightButtonFromPageIndex(_ index: Int) {
+        pushActivities()
+    }
+
+
 }
